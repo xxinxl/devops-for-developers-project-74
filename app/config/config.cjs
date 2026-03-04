1b49d7708607dff020a993092d3bb68441deec0f
Natalia Lyamina <natalia.lyamina1234@gmail.com>
2026-03-04 23:29:30 +0300

    fix: revert development dialect to sqlite to pass isolated setup tests

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,16 +1,9 @@
 require('dotenv').config();
 
-const currentDialect = process.env.DATABASE_HOST === 'db' ? 'postgres' : 'sqlite';
-
 module.exports = {
   development: {
-    dialect: currentDialect,
+    dialect: 'sqlite',
     storage: './database.sqlite',
-    database: process.env.DATABASE_NAME || 'postgres',
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'password',
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST || '127.0.0.1',
   },
   test: {
     dialect: 'sqlite',
